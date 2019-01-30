def6f610c26787cc756c061378a53fc537e032fd
ookamiyoujyo <ookamiyoujyo@gmail.com>
2019-01-30 12:19:06 +0900

    Initial commit

---
+++ Assets/cameras.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameras : MonoBehaviour
+{
+    public float spinSpeed = 0.5f;
+    float distance = 10f;
+
+    Vector3 pos = Vector3.zero;
+    public Vector2 mouse = Vector2.zero;
+
+    void Start()
+    {
+        mouse.x = 0.5f;
+        mouse.y = -0.5f;
+    }
+
+    void Update()
+    {
+        mouse += new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")) * Time.deltaTime * spinSpeed;
+        mouse.y = Mathf.Clamp(mouse.y, -0.3f + 0.5f, 0.3f + 0.5f);
+
+        // 球面座標系変換
+        pos.x = distance * Mathf.Sin(mouse.y * Mathf.PI) * Mathf.Cos(mouse.x * Mathf.PI);
+        pos.y = -distance * Mathf.Cos(mouse.y * Mathf.PI);
+        pos.z = -distance * Mathf.Sin(mouse.y * Mathf.PI) * Mathf.Sin(mouse.x * Mathf.PI);
+
+        // 座標の更新
+        transform.LookAt(pos + this.transform.position);
+    }
+
+}
+
